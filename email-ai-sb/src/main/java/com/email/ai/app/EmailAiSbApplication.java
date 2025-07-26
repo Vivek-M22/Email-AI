96665c116f22c53efe9fc888d6a780209d9bfd75
Vivek Makwana <vivekmakwana.one@gmail.com>
2025-07-26 11:56:00 +0530

    Project Setup

---
+++ email-ai-sb/src/main/java/com/email/ai/app/EmailAiSbApplication.java
@@ -0,0 +1,13 @@
+package com.email.ai.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAiSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAiSbApplication.class, args);
+	}
+
+}
---
+++ email-ai-sb/src/test/java/com/email/ai/app/EmailAiSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.ai.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAiSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
